6042d547d2f247aa97dc85c802470f043b25f8f9
Rubén Delicado <131898372+rdelicado@users.noreply.github.com>
2025-08-30 13:39:36 +0000

    add

--- arp.c
+++ arp.c
@@ -1,6 +1,6 @@
 #include <sys/socket.h>
 #include <linux/if_packet.h>
-#include <net/ehthernet.h>
+#include <net/ethernet.h>
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -13,7 +13,7 @@
 
 unsigned char MacOrigen[6], ipOrigen[4], MaskO[4];
 
-https://www.youtube.com/watch?v=O1y7xBC_6Ns&ab_channel=CodersESCOM
+// https://www.youtube.com/watch?v=O1y7xBC_6Ns&ab_channel=CodersESCOM
 int openSocket()
 {
     int packet_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
@@ -27,7 +27,7 @@ int getIndex(int fd)
 
     printf("Inserta el nombre de la interfaz: ");
     scanf("%s", nic.ifr_name);
-    if (ioctl(ds, SIOCGIFINDEX, &nic) == -1) {
+    if (ioctl(fd, SIOCGIFINDEX, &nic) == -1) {
         system("clear");
         perror("Error en el indes\n");
         exit(1);
@@ -37,9 +37,27 @@ int getIndex(int fd)
     return index;
 }
 
-void getData(int ds)
+void getData(int fd)
 {
-    struct
+    struct ifreq nic;
+    if (ioctl(fd, SIOCGIFHWADDR, &nic) == -1) {
+        perror("Error al encontrar la MAC\n");
+    } else {
+        memcpy(MacOrigen, nic.ifr_hwaddr.sa_data, 6);
+        printf("\nLa MAC del dispostivo es: ");
+        printf("%.2x:%.2x:%.2x:%.2x:%.2x:%.2x\n", 
+            MacOrigen[0], MacOrigen[1], MacOrigen[2], 
+            MacOrigen[3], MacOrigen[4], MacOrigen[5]);
+    }
+    
+    if (ioctl(fd, SIOCGIFADDR, &nic) == -1) {
+        perror("Error al encontrar la IP\n");
+    } else {
+        memcpy(ipOrigen, nic.ifr_hwaddr.sa_data + 2, 4);
+        printf("\nIp de dispositivo: ");
+        printf("%d.%d.%d.%d\n", ipOrigen[0], ipOrigen[1], ipOrigen[2], ipOrigen[3]);
+    }
+
 }
 
 int main()
@@ -52,7 +70,7 @@ int main()
         exit(1);
     } else {
         system("clear");
-        printf("Socket abierto\n")
+        printf("Socket abierto\n");
         index = getIndex(fd);
         printf("Index: %i\n", index);
         getData(fd);
